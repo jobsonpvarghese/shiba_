4d21b7df76df4ac4046987ab7a4ca4b6b97ab148
jobsonpvarghese <jobsonvarghese1@gmail.com>
2024-05-15 02:11:03 -0400

    drawer changed

--- src/components/drawer/RightDrawer.jsx
+++ src/components/drawer/RightDrawer.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Drawer } from "@mui/material"
-import "./RightDrawer.css"
+// import "./RightDrawer.css"
 
 const RightDrawer = ({ children, open, onClose, onOpen, className }) => {
   const toggleDrawer = open => event => {
@@ -10,9 +10,24 @@ const RightDrawer = ({ children, open, onClose, onOpen, className }) => {
     onClose(open)
   }
 
+  const drawerWidth = 240
   return (
     <div>
-      <Drawer elevation={100} anchor={"right"} open={open} onClose={toggleDrawer(false)}>
+      <Drawer
+        elevation={100}
+        anchor={"right"}
+        open={open}
+        onClose={toggleDrawer(false)}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+            backgroundColor: "#0e46a3" // Change this to your desired color
+          }
+        }}
+      >
         {children}
       </Drawer>
     </div>
