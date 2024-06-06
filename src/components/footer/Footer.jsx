69b67bf98595e7e366ae07d8b775722ca40d4bac
jobsonpvarghese <jobsonvarghese1@gmail.com>
2024-06-06 14:28:12 -0400

    .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,22 +1,26 @@
 import React from "react"
 import { Grid, Typography, Box } from "@mui/material"
 import "./footer.css"
-import logo from "../../images/logo.png"
+import logo from "../../images/logofooterdark.png"
+import logofooter from "../../images/footer-logo.png"
 import tg from "../../images/icons/telegram.png"
 
 const Footer = () => {
   return (
     <div className="footer-container">
+      <div class="image-container">
+        <img src={logofooter} alt="logofooter" className="logofooter" />
+      </div>
       <Box component="footer">
         <Grid container spacing={10}>
           <Grid item xs={12} md={6}>
             <Typography variant="h6">About Us</Typography>
             <img src={logo} alt="logo" className="logo-foot" />
-            <Typography variant="body2">
+            <p>
               Vote4Shiba is not your ordinary memecoin. It combines the whimsical charm and viral potential of a memecoin with groundbreaking features
               that foster community engagement and governance. At the heart of Vote4Shiba lies a strong sense of community attachment and a commitment
               to advocating for free speech.
-            </Typography>
+            </p>
           </Grid>
           <Grid item xs={12} md={6}>
             <Typography variant="h6" sx={{ pb: 2 }}>
