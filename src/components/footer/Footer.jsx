d7b1247037fbc6b8b4bb9d2be8254e3329a3041d
jobsonpvarghese <jobsonvarghese1@gmail.com>
2024-06-06 14:05:26 -0400

    clr

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,44 +1,39 @@
 import React from "react"
-import { Grid, Typography, Link, Box } from "@mui/material"
+import { Grid, Typography, Box } from "@mui/material"
+import "./footer.css"
+import logo from "../../images/logo.png"
+import tg from "../../images/icons/telegram.png"
 
 const Footer = () => {
   return (
-    <Box component="footer" sx={{ p: 3, backgroundColor: "#b22234", color: "white" }}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={6} md={4}>
-          <Typography variant="h6">About Us</Typography>
-          <Typography variant="body2">
-            Vote4Shiba is not your ordinary memecoin. It combines the whimsical charm and viral potential of a memecoin with groundbreaking features
-            that foster community engagement and governance. At the heart of Vote4Shiba lies a strong sense of community attachment and a commitment
-            to advocating for free speech.
-          </Typography>
+    <div className="footer-container">
+      <Box component="footer">
+        <Grid container spacing={10}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6">About Us</Typography>
+            <img src={logo} alt="logo" className="logo-foot" />
+            <Typography variant="body2">
+              Vote4Shiba is not your ordinary memecoin. It combines the whimsical charm and viral potential of a memecoin with groundbreaking features
+              that foster community engagement and governance. At the heart of Vote4Shiba lies a strong sense of community attachment and a commitment
+              to advocating for free speech.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" sx={{ pb: 2 }}>
+              Follow Us
+            </Typography>
+            <div className="icon-item">
+              <img src={tg} alt="tg" className="icon" />
+              <a href="https://facebook.com">Telegram</a>
+            </div>
+            <div className="icon-item">
+              <img src={tg} alt="tg" className="icon" />
+              <a href="https://facebook.com">Telegram</a>
+            </div>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Typography variant="h6">Contact</Typography>
-          <Typography variant="body2">
-            Email:{" "}
-            <Link sx={{ color: "white" }} href="mailto:contact@vote4shiba.com">
-              contact@vote4shiba.com
-            </Link>
-          </Typography>
-        </Grid>
-        <Grid item xs={12} sm={6} md={4}>
-          <Typography variant="h6">Follow Us</Typography>
-          <Link sx={{ color: "white" }} href="https://facebook.com" target="_blank" rel="noopener" variant="body2" display="block">
-            Facebook
-          </Link>
-          <Link sx={{ color: "white" }} href="https://twitter.com" target="_blank" rel="noopener" variant="body2" display="block">
-            Twitter
-          </Link>
-          <Link sx={{ color: "white" }} href="https://instagram.com" target="_blank" rel="noopener" variant="body2" display="block">
-            Instagram
-          </Link>
-          <Link sx={{ color: "white" }} href="https://linkedin.com" target="_blank" rel="noopener" variant="body2" display="block">
-            LinkedIn
-          </Link>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
+    </div>
   )
 }
 
