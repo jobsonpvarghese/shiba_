3d847779af79796cb500fb6f3070e26d0f66014c
jobsonpvarghese <jobsonvarghese1@gmail.com>
2024-06-14 22:52:18 -0400

    counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -7,7 +7,7 @@ import CountdownTimer from "./CounterComponent.jsx"
 const theme = createTheme()
 
 const Counter = () => {
-  const targetDate = new Date("June 30, 2024 00:09:00").getTime() // Ensure target date is in milliseconds
+  const targetDate = new Date("August 15, 2024 00:09:00").getTime() // Ensure target date is in milliseconds
 
   return (
     <ThemeProvider theme={theme}>
